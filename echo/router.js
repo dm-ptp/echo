2de8b275576a2605cfff0dd9263561d425ec1172
Patrick Putnam <patrick.putnam@demandmedia.com>
2014-12-15 16:44:05 -0800

    Add handling for loader.io endpoint validation.

--- echo/router.js
+++ echo/router.js
@@ -2,6 +2,7 @@ function route(pathname) {
 	if(pathname.substr(1,11) == 'favicon.ico') { return 'favicon' };
 	console.log("Routing request to " + pathname);
 	if(pathname.substr(1).match(/^api/)) { return 'api' };
+	if(pathname.substr(1).match(/^loaderio/)) { return 'loaderio' };
 	return 'no route';
 }
 
--- echo/server.js
+++ echo/server.js
@@ -1,5 +1,7 @@
+var fs = require("fs");
 var http = require("http");
 var url = require("url");
+var LOADERIO_VERIFICATION_FILE = 'external/loaderio.verify'
 
 function start(route) {
 	function onRequest(request, response) {
@@ -15,7 +17,21 @@ function start(route) {
 		if(handler == 'no route'){
 			response.writeHead(200, {"Content-Type": "text/plain"});
 			response.write("No route found for " + pathname + "\n");
+			response.end();
 		} else if(handler == 'favicon'){
+		} else if(handler == 'loaderio'){
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			f = fs.readFile(LOADERIO_VERIFICATION_FILE, 'utf8', function (error, data) {
+				if (error) {
+					response.write("Could not load loaderio verification data: " + error);
+					response.end();
+				} else {
+					data = data.replace(/\n.*/, '');
+					response.write(data); 
+					console.log("Printing loaderio key: " + data);
+					response.end();
+				}
+			});
 		} else if(handler == 'api'){
 			response.writeHead(200, {"Content-Type": "text/json"});
 			console.log("Kicking off api handler");
@@ -23,11 +39,12 @@ function start(route) {
 				console.log("Got a legit query");
 				response.write(JSON.stringify(query, null, 2));
 			} else { response.write("No query parameters\n") };
+			response.end();
 		} else {
 			response.write("Error getting response from router.\n");
+			response.end();
 		};
 
-		response.end();
 	}
 
 	http.createServer(onRequest).listen(8888);
